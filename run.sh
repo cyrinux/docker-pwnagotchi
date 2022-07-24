d28c658ebcf3728313f983bf5731661c192700c0
Cyril Levis <git@levis.name>
2022-07-24 18:44:29 +0200

    chore: set IMAGE_NAMESPACE with envrc

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 set -eu
 export LC_ALL=C
 
-DOCKER=$(command -v docker 2>/dev/null)
+DOCKER=$(command -v docker 2> /dev/null)
 
 IMAGE_REGISTRY=docker.io
-IMAGE_NAMESPACE=hectorm
+IMAGE_NAMESPACE=${IMAGE_NAMESPACE:-hectorm}
 IMAGE_PROJECT=pwnagotchi
 IMAGE_TAG=latest
 IMAGE_NAME=${IMAGE_REGISTRY:?}/${IMAGE_NAMESPACE:?}/${IMAGE_PROJECT:?}:${IMAGE_TAG:?}
@@ -16,19 +16,19 @@ imageExists() { [ -n "$("${DOCKER:?}" images -q "${1:?}")" ]; }
 containerExists() { "${DOCKER:?}" ps -af name="${1:?}" --format '{{.Names}}' | grep -Fxq "${1:?}"; }
 containerIsRunning() { "${DOCKER:?}" ps -f name="${1:?}" --format '{{.Names}}' | grep -Fxq "${1:?}"; }
 
-if ! imageExists "${IMAGE_NAME:?}" && ! imageExists "${IMAGE_NAME#docker.io/}"; then
-	>&2 printf '%s\n' "\"${IMAGE_NAME:?}\" image doesn't exist!"
+if ! imageExists "${IMAGE_NAME:?}" && ! imageExists "${IMAGE_NAME#$IMAGE_REGISTRY/}"; then
+	printf >&2 '%s\n' "\"${IMAGE_NAME:?}\" image doesn't exist!"
 	exit 1
 fi
 
 if containerIsRunning "${CONTAINER_NAME:?}"; then
 	printf '%s\n' "Stopping \"${CONTAINER_NAME:?}\" container..."
-	"${DOCKER:?}" stop "${CONTAINER_NAME:?}" >/dev/null
+	"${DOCKER:?}" stop "${CONTAINER_NAME:?}" > /dev/null
 fi
 
 if containerExists "${CONTAINER_NAME:?}"; then
 	printf '%s\n' "Removing \"${CONTAINER_NAME:?}\" container..."
-	"${DOCKER:?}" rm "${CONTAINER_NAME:?}" >/dev/null
+	"${DOCKER:?}" rm "${CONTAINER_NAME:?}" > /dev/null
 fi
 
 printf '%s\n' "Creating \"${CONTAINER_NAME:?}\" container..."
@@ -44,7 +44,7 @@ printf '%s\n' "Creating \"${CONTAINER_NAME:?}\" container..."
 	--env PWNAGOTCHI_PLUGIN_LED_ENABLED=false \
 	--mount type=tmpfs,dst=/run/,tmpfs-mode=0755 \
 	--mount type=tmpfs,dst=/tmp/,tmpfs-mode=1777 \
-	"${IMAGE_NAME:?}" "$@" >/dev/null
+	"${IMAGE_NAME:?}" "$@" > /dev/null
 
 printf '%s\n\n' 'Done!'
 exec "${DOCKER:?}" logs -f "${CONTAINER_NAME:?}"
