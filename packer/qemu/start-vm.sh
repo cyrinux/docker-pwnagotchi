ea122acff174138c6fe44d9c484f9d46be67f9ad
Cyril Levis <git@levis.name>
2022-07-25 23:51:27 +0200

    chore: wip

--- packer/qemu/start-vm.sh
+++ packer/qemu/start-vm.sh
@@ -7,7 +7,8 @@ SRC_DIR=$(CDPATH='' cd -- "$(dirname -- "$(dirname -- "${0:?}")")" && pwd -P)
 TMP_DIR=$(mktemp -d)
 
 ARCH=${1-armhf}
-DISK=${SRC_DIR:?}/dist/${ARCH:?}/pwnagotchi.img
+PWNAGOTCHI_NAME=${PWNAGOTCHI_NAME:-pwnagotchi}
+DISK=${SRC_DIR:?}/dist/${ARCH:?}/disk-${PWNAGOTCHI_NAME:?}.img
 
 RPI_KERNEL_URL='https://raw.githubusercontent.com/raspberrypi/firmware/1.20200601/boot/kernel8.img'
 RPI_KERNEL_FILE=${2-${TMP_DIR:?}/kernel.img}
@@ -21,7 +22,7 @@ trap 'ret="$?"; rm -rf -- "${TMP_DIR:?}"; trap - EXIT; exit "${ret:?}"' EXIT TER
 
 # Download RPI kernel and DTB
 [ -e "${RPI_KERNEL_FILE:?}" ] || curl --proto '=https' --tlsv1.2 -Lo "${RPI_KERNEL_FILE:?}" "${RPI_KERNEL_URL:?}"
-[ -e "${RPI_DTB_FILE:?}"    ] || curl --proto '=https' --tlsv1.2 -Lo "${RPI_DTB_FILE:?}"    "${RPI_DTB_URL:?}"
+[ -e "${RPI_DTB_FILE:?}" ] || curl --proto '=https' --tlsv1.2 -Lo "${RPI_DTB_FILE:?}" "${RPI_DTB_URL:?}"
 
 # Launch VM
 qemu-system-aarch64 \
