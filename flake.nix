0507999aebbf33770aa2bbddf450fd85d14fa960
Cyril Levis <git@levis.name>
2024-09-14 17:45:00 +0200

    chore: wip

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let systems = [ "x86_64-linux" "aarch64-linux" ];
+    in flake-utils.lib.eachSystem systems (system:
+      let
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            m4
+            packer
+            envsubst
+            gnumake
+            docker
+          ];
+        };
+      }
+    );
+}
